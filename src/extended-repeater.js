69a0c1e7ebe10d79ff971696921086ae26a2024a
MyAngelRem <vlad_palamarchuk_01@mail.ru>
2021-03-11 14:37:25 +0300

    complete: complete extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  const finalOptions = {
+    repeatTimes: options.repeatTimes ? options.repeatTimes : 1,
+    separator: options.separator ? options.separator : '+',
+    addition: options.addition || typeof(options.addition) === 'boolean' || options.addition === null ? options.addition : '', // ¯\_(ツ)_/¯
+    additionRepeatTimes: options.additionRepeatTimes ? options.additionRepeatTimes : 1,
+    additionSeparator: options.additionSeparator ? options.additionSeparator : '|'
+  };
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let strReps = [];
+  const sepReps = [];
+
+  for (let i = 0; i < finalOptions.repeatTimes; i++) strReps.push(str);
+  for (let i = 0; i < finalOptions.additionRepeatTimes; i++) sepReps.push(`${finalOptions.addition}`);
+
+  const addSepStr = sepReps.join(finalOptions.additionSeparator);
+  strReps = strReps.map((e) => e + addSepStr);
+
+  return strReps.join(finalOptions.separator);
 };
   
