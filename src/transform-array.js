a552c4efa4798c58b8cd19e29e81a189365bf3c0
MyAngelRem <vlad_palamarchuk_01@mail.ru>
2021-03-12 17:37:17 +0300

    complete: complete transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  
+  const controls = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev'
+  ];
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return arr.reduce((acc, cur, index) => {
+
+    if (controls.includes(cur)) return acc;
+    if (arr[index - 1] === '--discard-next') return acc;
+    
+    if (arr[index - 1] === '--double-next') acc.push(cur, cur);
+    else acc.push(cur);
+
+    if (arr[index + 1] === '--discard-prev') acc.splice(acc.length - 1, 1);
+    if (arr[index + 1] === '--double-prev') acc.push(cur);
+
+    return acc;
+  }, []);
 };
